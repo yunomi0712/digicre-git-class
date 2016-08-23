ea2ddec285af41b861a64f7731381cb3af2117f3
yunomi <yunomi0712@outlook.jp>
2016-08-23 10:04:17 +0900

    偶数表示を追加

--- main.c
+++ main.c
@@ -4,5 +4,10 @@ int main(void){
     for(int i = 0;i < 10;i++){
         printf("%d\n",i*i);
     }
+    for(int i = 0;i < 100;i++){
+        if(i % 2 == 0) {
+            printf("%d\n",i);
+        }
+    }
     return 0;
 }
